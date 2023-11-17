fa4e527247f0730df4e4fae9ea3d54b8dd7655d2
vSolomony <mohamed.a.kassem95@gmail.com>
2023-11-17 13:32:57 -0800

    123

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,26 +7,27 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_idx;
+	size_t i, j, k;
 	int temp;
 
-	if (array == NULL || size < 2)
+	if (array == NULL || !size)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min_idx = i;
-
-		for (j = i + 1; j < size; j++)
+		for (j = size - 1, k = i + 1 ; j > i; j--)
 		{
-			if (array[j] < array[min_idx])
-				min_idx = j;
+			if (array[j] < array[k])
+			{
+				k = j;
+			}
 		}
-
-		temp = array[i];
-		array[i] = array[min_idx];
-		array[min_idx] = temp;
-
-		print_array(array, size);
+		if (array[i] > array[k])
+		{
+			temp = array[i];
+			array[i] = array[k];
+			array[k] = temp;
+			print_array(array, size);
+		}
 	}
 }
